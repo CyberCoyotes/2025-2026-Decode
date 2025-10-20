781d7c791f079acbcda2e300ac7a8f1680b44a34
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2025-10-20 09:13:04 -0400

    Add ScoringSubsystem.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystems/ScoringSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystems/ScoringSubsystem.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode.common.subsystems;
 
+/**
+ * Components of the Scoring Subsystem:
+ * - Turret rotation with a goBilda position based servo
+ * - Hood Adjustment with a goBilda position based servo
+ * - Ball launcher-flywheel with a goBilda motor
+ */
+
 public class ScoringSubsystem {
-}
+
+
+} // end of class
