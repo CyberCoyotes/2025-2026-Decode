9c1646e331684401408ccac2a82175d19808c596
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2025-11-28 10:24:45 -0500

    Added IndexSubsystem imports to autons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/auton/BlueLongDriveShoot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/auton/BlueLongDriveShoot.java
@@ -10,6 +10,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.common.subsystems.MecanumDriveSubsystem;
 import org.firstinspires.ftc.teamcode.common.subsystems.PinpointOdometrySubsystem;
 import org.firstinspires.ftc.teamcode.common.subsystems.ShooterSubsystem;
+import org.firstinspires.ftc.teamcode.common.subsystems.IndexSubsystem;
+
 
 /**
  * Complex Autonomous OpMode for Team 11940
@@ -29,6 +31,8 @@ public class BlueLongDriveShoot extends LinearOpMode {
     private MecanumDriveSubsystem drive;
     private PinpointOdometrySubsystem odometry;
     private ShooterSubsystem shooter;
+    private IndexSubsystem index;
+
 
     // Movement configuration
     private static final double DRIVE_SPEED = 0.4; // 40% power for distance moves
