babb78e44ff5b05f99fe17ebc91b1a920c7a2a7b
JediScoy <coyotepod@gmail.com>
2026-05-23 11:31:17 -0400

    Add Pedro Pathing visualizer path file (placeholder coordinates)

---
+++ paths/prime-auton.pp
@@ -0,0 +1,55 @@
+{
+  "startPoint": {
+    "x": 9,
+    "y": 63,
+    "heading": "constant",
+    "startDeg": 90,
+    "endDeg": 90,
+    "locked": false
+  },
+  "lines": [
+    {
+      "id": "line-to-score",
+      "name": "To Score",
+      "endPoint": {
+        "x": 9,
+        "y": 84,
+        "heading": "constant",
+        "startDeg": 90,
+        "endDeg": 90,
+        "locked": false
+      },
+      "controlPoints": [],
+      "color": "#ffc516",
+      "locked": false,
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "line-to-park",
+      "name": "To Park",
+      "endPoint": {
+        "x": 36,
+        "y": 84,
+        "heading": "constant",
+        "startDeg": 90,
+        "endDeg": 90,
+        "locked": false
+      },
+      "controlPoints": [],
+      "color": "#22c55e",
+      "locked": false,
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    }
+  ],
+  "shapes": [],
+  "sequence": [],
+  "pathChains": [],
+  "version": "1.2.1",
+  "timestamp": "2026-05-23T00:00:00.000Z"
+}
