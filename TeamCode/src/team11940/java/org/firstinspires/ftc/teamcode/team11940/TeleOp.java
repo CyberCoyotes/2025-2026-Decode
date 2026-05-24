ed164fe6595e6663e62585a65c446b8fc57f4f99
JediScoy <coyotepod@gmail.com>
2026-05-24 09:24:16 -0400

    factor teleop

--- TeamCode/src/team11940/java/org/firstinspires/ftc/teamcode/team11940/TeleOp.java
+++ TeamCode/src/team11940/java/org/firstinspires/ftc/teamcode/team11940/TeleOp.java
@@ -2,10 +2,10 @@ package org.firstinspires.ftc.teamcode.team11940;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.common.config.RobotConfig;
-import org.firstinspires.ftc.teamcode.common.teleop.PrimeTeleOpBase;
+import org.firstinspires.ftc.teamcode.common.teleop.TeleOpBase;
 
 @TeleOp(name = "Team 11940 TeleOp", group = "TeleOp")
-public class PrimeTeleOp extends PrimeTeleOpBase {
+public class TeleOp extends TeleOpBase {
     @Override
     protected RobotConfig getRobotConfig() { return new Team11940Config(); }
 }
--- TeamCode/src/team22091/java/org/firstinspires/ftc/teamcode/team22091/TeleOp.java
+++ TeamCode/src/team22091/java/org/firstinspires/ftc/teamcode/team22091/TeleOp.java
@@ -2,10 +2,10 @@ package org.firstinspires.ftc.teamcode.team22091;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.common.config.RobotConfig;
-import org.firstinspires.ftc.teamcode.common.teleop.PrimeTeleOpBase;
+import org.firstinspires.ftc.teamcode.common.teleop.TeleOpBase;
 
 @TeleOp(name = "Team 22091 TeleOp", group = "TeleOp")
-public class PrimeTeleOp extends PrimeTeleOpBase {
+public class TeleOp extends TeleOpBase {
     @Override
     protected RobotConfig getRobotConfig() { return new Team22091Config(); }
 }
